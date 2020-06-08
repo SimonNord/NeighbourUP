70b9606251bc01a0f63f1522fcace617d0bd038e
Simon <simon.nordstrom@chasacademy.se>
2020-06-08 11:12:32 +0200

    add config for absolute imports

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import Colors from 'variables';
 import Title from './components/Title';
-import Colors from '../../variables';
 
 const StyledForm = styled.form`
   display: flex;
